cc78ffa2e4bf17f16e3d38fca439a91d02938e1e
kajal1801 <anshikajais2001@gmail.com>
2022-05-10 22:46:22 +0530

    STL Practice

---
+++ STL.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Vector
+//vector<int> v;
+//vector<int> v(n);
+//vector<int> v(n,0); Vector of integers containing 0
+//vector<int> v(v1); Copy vector v1
+//v.size(); Returns the size of the vector [O(1)]
+//v.capacity(); Returns the capacity of the vector
+//v.push_back(x); Adds x to the end of the vector [O(1)]
+//v.pop_back(); Removes the last element of the vector [O(1)]
+//v.clear(); Removes all elements of the vector [O(n)]
+
+int main(){
+    // vector<int> v;
+    // v.push_back(5);
+    // v.push_back(10);
+    // v.push_back(15);
+
+    // cout << v[0] << " " << v[1] << " " << v[2] << endl;
+
+    // v.pop_back();
+    // cout << v[0] << " " << v[1] << endl;
+    // v.push_back(0);
+
+    // cout<< v[0] << " " << v[1] << " " << v[2] << endl;
+
+
+
+    // int arr[] = {45, 76, 32, 12, 5, 9, 10, 7, 6, 8};
+    // sort(arr, arr+10);
+
+    // for(int i = 0;i<10;i++){
+    //     cout << arr[i] << " ";
+    // }
+
+
+    
+
+}
