fbeaa8b53828c93c346db1ad4ec2fa40d17c4bd7
kajal1801 <anshikajais2001@gmail.com>
2022-07-09 14:51:30 +0530

    Codechef CCHOCOLATES

---
+++ CChocolates.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    int sum = (x*5)+(y*10);
+	    int ans = sum/z;
+	    cout<<ans<<"\n";
+	}
+	return 0;
+}
