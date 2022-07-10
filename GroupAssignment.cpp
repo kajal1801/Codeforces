4ea0a5171aa12f7b168dff512dd33376d54b3e32
kajal1801 <anshikajais2001@gmail.com>
2022-07-10 15:02:36 +0530

    Codechef GRPASSN

---
+++ GroupAssignment.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+    cin>>t;
+	while(t--){
+	    int n,i;
+	    cin>>n;
+	    int arr[n];
+	    int p[n+1];
+        std::fill(p,p+n+1,0);
+	    for(i = 0;i<n;i++){
+	        cin>>arr[i];
+	        p[arr[i]] += 1;
+	    }
+	    bool f = true;
+	    for(i = 2;i<=n;i++){
+	        if(p[i]%i != 0){
+	            f = false;
+	            break;
+	        }
+	    }
+	    if(f){
+	        cout<<"YES"<<"\n";
+	    }
+	    else{
+	        cout<<"NO"<<"\n";
+	    }
+	}
+	return 0;
+}
