30a0e85a5390cd4d1747ba35c8d16d31189b6bd9
kajal1801 <anshikajais2001@gmail.com>
2022-07-10 15:59:31 +0530

    Codechef MUFFINS3

---
+++ PackagingCupcakes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int ans = (n/2)+1;
+	    cout<<ans<<"\n";
+	}
+	return 0;
+}
